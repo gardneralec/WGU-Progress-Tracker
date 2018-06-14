f2818d30db4afa554cff143e86150f5e02a4d660
Alec Gardner <alexander.gardner731@gmail.com>
2018-06-14 09:26:34 -0500

    Initial Commit

---
+++ app/src/main/java/com/alecgardner/wgu_progress_tracker/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.alecgardner.wgu_progress_tracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
